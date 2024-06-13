1afd93ac200feb967933e06bd6438bb3d2571c0a
Adam Kopiec <adas073@gmail.com>
2024-06-13 05:16:25 +0200

    extend deadline + singleton

---
+++ ParcelDeliveryService/Core/ParcelStates/DeadlineExtendedState.cs
@@ -0,0 +1,18 @@
+﻿using ParcelDeliveryService.Commands;
+using ParcelDeliveryService.Interfaces;
+using ParcelDeliveryService.Models;
+
+namespace ParcelDeliveryService.Core.ParcelStates
+{
+    public class DeadlineExtendedState : ParcelState
+    {
+        public override bool IsTerminalState => false;
+
+        public override void HandleForwardInTransit(Parcel parcel, IParcelService parcelService, ILockerService lockerService)
+        {
+            var command = new StoreParcelExternallyCommand(parcelService, lockerService);
+            command.Execute(parcel);
+        }
+
+    }
+}
--- ParcelDeliveryService/Program.cs
+++ ParcelDeliveryService/Program.cs
@@ -9,10 +9,24 @@ namespace ParcelDeliveryService
     internal class Program
     {
         private readonly IMenu _mainMenu;
+        private static Program? _instance;
+        private static readonly object Lock = new();
 
-        public Program(IMenu mainMenu)
+        private Program(IMenu mainMenu)
         {
             _mainMenu = mainMenu;
+        }
+
+        public static Program Instance(IMenu mainMenu)
+        {
+            lock (Lock)
+            {
+                if (_instance == null)
+                {
+                    _instance = new Program(mainMenu);
+                }
+                return _instance;
+            }
         }
 
         public void Run()
@@ -32,18 +46,13 @@ namespace ParcelDeliveryService
             var lockerService = new LockerService(lockerRepository);
             var rerouteService = new RerouteServiceDecorator(parcelService);
 
-            var userPortalMenu = new UserPortalMenu(parcelService, lockerService,lockerRepository, rerouteService, userService);
+            var userPortalMenu = new UserPortalMenu(parcelService, lockerService, lockerRepository, rerouteService, userService);
             var lockerMenu = new LockerMenu(lockerService, parcelService);
-
             var transitMenu = new TransitMenu(parcelService, lockerService);
 
-            var program = new Program(
-                new MainMenu(
-                    userPortalMenu,
-                    lockerMenu,
-                    transitMenu
-                )
-            );
+            var mainMenu = new MainMenu(userPortalMenu, lockerMenu, transitMenu);
+
+            var program = Instance(mainMenu);
 
             program.Run();
         }
